a2ead1749c88ecd4132e76f46fc2284002de7cc9
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2018-10-19 00:33:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(numOfFeet){
   if(numOfFeet<=400){
     return 'This one is on me!'
-  }else if(numOfFeet>2000){
+  }else if(numOfFeet>2000 &&numOfFeet<2500){
     return 'I will gladly take your thirty bucks.'
   }
 }
